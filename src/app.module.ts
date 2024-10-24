38e3cf31292762525598233ab468dfb13a0abac4
neyjh <ney@jinhakapply.com>
2024-10-24 11:35:45 +0900

    feat: diary 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { DiaryModule } from './diary/diary.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, DiaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
